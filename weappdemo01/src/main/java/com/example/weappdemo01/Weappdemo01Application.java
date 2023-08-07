0ce9b8d9407a21e5a07e33521dd80a4dec7110c7
the-zzy <110391272+the-zzy@users.noreply.github.com>
2023-08-07 15:11:46 +0800

    Add files via upload

---
+++ weappdemo01/src/main/java/com/example/weappdemo01/Weappdemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.weappdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weappdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Weappdemo01Application.class, args);
+    }
+
+}
